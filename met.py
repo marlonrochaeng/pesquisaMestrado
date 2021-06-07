4016dacf4ae0a0c5c5591f1628a6d10ec91d7a55
Marlon Rocha <marlon.univasf@gmail.com>
2021-06-07 10:54:35 -0300

    1 commit

---
+++ met.py
@@ -0,0 +1,14 @@
+import numpy as np
+from utils import Utils
+
+jobs = 512 
+resources = 16
+
+u = Utils()
+
+ET, CT, maquinas = u.initialize('u_c_lolo.0', jobs, resources)
+
+
+res = u.met(ET, CT, maquinas)
+res.sort()
+print("Maquinas: ",res)
